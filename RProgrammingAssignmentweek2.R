6964f06f7bfc7ce426052f3e201e778ebc637723
ravikanth K <ravikanth.katikitala@gmail.com>
2020-11-24 01:44:34 +0530

    function 1

--- RProgrammingAssignmentweek2.R
+++ RProgrammingAssignmentweek2.R
@@ -1,15 +1,25 @@
 pt<- 'C:/Users/Ravikanth/Documents/data Samples/rprog_data_specdata/specdata'
 
 pollutantmean<-function(directory, pollutant,id){
-                  meanTotal<-0
-                  for(i in id)
-                  {
-                          newpath<- paste(directory,'/',sprintf("%03d",i),'.csv',sep="")
-                          df<-read.csv(newpath) 
-                          df_pollutant<-df[pollutant]
-                          df<-df_pollutant[!is.na(df_pollutant)]
-                          meanTotal<- meanTotal+mean(df)
-                  }
-                 meanTotal
-        
+        meanTotal<-0
+        old_df<-NULL
+        for(i in id)
+        {
+                newpath<- paste(directory,'/',sprintf("%03d",i),'.csv',sep="")
+                df<-read.csv(newpath) 
+                df_pollutant<-df[pollutant]
+                vec<-!is.na(df[pollutant])
+                new_df<- data.frame(df_pollutant[vec])
+                if(!is.null(old_df))
+                {
+                        df<- rbind(old_df,new_df)  
+                }
+                else
+                {
+                   df<-new_df     
+                }
+                old_df<-df
+               
+        }
+        mean(as.vector(t(df)))
 }
